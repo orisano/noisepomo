bbd4de4662a93d6aaea136c39ba0523248f227d7
Nao Yonashiro <owan.orisano@gmail.com>
2017-06-16 23:02:28 +0900

    style: use jsfmt

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
     const z = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     const k = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     const N = z.length;
-
     k[N - 1] = 0.5;
     for (let i = N - 1; i >= 1; i--) {
       k[i - 1] = k[i] * 0.25;
@@ -15,7 +14,7 @@
     while (true) {
       let x = [-1, 1][Math.random() * 2 | 0];
       for (let i = 0; i < N; i++) {
-        z[i] = (x * k[i] + z[i] * (1.0 - k[i])); 
+        z[i] = (x * k[i] + z[i] * (1.0 - k[i]));
         x = (x + z[i]) * 0.5;
       }
       out = 0.75 * x + 0.25 * out;
@@ -52,6 +51,38 @@
 
     $startButton.addEventListener("click", work);
     $stopButton.addEventListener("click", rest);
+
+    const $action = document.getElementById("action");
+    const opt = {
+      duration: 2000
+    };
+    let flag = true;
+    $action.addEventListener("click", () => {
+      if (flag) {
+        KUTE.fromTo("#left", {
+          path: "#play-left"
+        }, {
+          path: "#pause-left"
+        }, opt).start();
+        KUTE.fromTo("#right", {
+          path: "#play-right"
+        }, {
+          path: "#pause-right"
+        }, opt).start();
+      } else {
+        KUTE.fromTo("#left", {
+          path: "#pause-left"
+        }, {
+          path: "#play-left"
+        }, opt).start();
+        KUTE.fromTo("#right", {
+          path: "#pause-right"
+        }, {
+          path: "#play-right"
+        }, opt).start();
+      }
+      flag = !flag;
+    });
   });
 })(
   navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia,
